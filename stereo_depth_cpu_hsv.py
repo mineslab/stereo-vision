fc10d211d798128b83bf75872dc7ea5d06a03771
asujaykk <akhillearningforhim@gmail.com>
2024-04-28 20:05:58 +0530

    correction w.r.t stereo calibate module

--- stereo_depth_cpu_hsv.py
+++ stereo_depth_cpu_hsv.py
@@ -15,13 +15,14 @@ def draw_lines(img):
 cam1 = jetCam.jetsonCam()
 cam2 = jetCam.jetsonCam()
 
-
+#left camera 
 cam1.open(sensor_id=1,
           sensor_mode=3,
           flip_method=0,
           display_height=540,
           display_width=960,
         )
+#right camera
 cam2.open(sensor_id=0,
           sensor_mode=3,
           flip_method=0,
@@ -33,7 +34,7 @@ cam1.start()
 cam2.start()
 #stereoCalibrateCamera(cam1,cam2,'jetson_stereo_8MP',24)
 lod_data = getStereoCameraParameters('jetson_stereo_8MP.npz')
-lod_datac1 = getStereoSingleCameraParametersStereoSingleCameraParameters('jetson_stereo_8MPc1.npz')
+lod_datac1 = getStereoSingleCameraParameters('jetson_stereo_8MPc1.npz')
 lod_datac2 = getStereoSingleCameraParameters('jetson_stereo_8MPc2.npz')
 
 print(lod_datac1[0])
